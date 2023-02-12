fix residual bug: (1)SphP[i].Energy when exchanging prim&gradients, (2) parenthesis in macro GAMMA=(5/3); add lapack for matrix inv

examples/gresho_2d/create.py
@@ -13,7 +13,7 @@ simulation_directory = str(sys.argv[1])
 print("examples/Gresho_2d/create.py: creating ICs in directory " + simulation_directory)
 
 """ initial condition parameters """
-FilePath = simulation_directory + '/IC_gresho_v0_50.hdf5'
+FilePath = simulation_directory + '/IC_gresho_v1e-12_50.hdf5'
 
 FloatType = np.float64  # double precision: np.float64, for single use np.float32
 IntType = np.int32
@@ -27,7 +27,7 @@ else:
 
 ## parameters
 density_0 = 1.0
-velocity_0 = 0.0 ## bulk velocity
+velocity_0 = 1.0e-12 ## bulk velocity
 gamma = 5.0/3.0
 gamma_minus_one = gamma - 1.0
 
@@ -85,6 +85,7 @@ i_all_but_central = np.arange(1,NumberOfCells)
 Vel[i_all_but_central,0] = RotationVelocity[i_all_but_central] * yPosFromCenter[i_all_but_central] / Radius[i_all_but_central]
 Vel[i_all_but_central,1] = -RotationVelocity[i_all_but_central] * xPosFromCenter[i_all_but_central] / Radius[i_all_but_central]
 Vel[:,0] += velocity_0
+Vel[:,1] += velocity_0
 
 ## specific internal energy
 Pressure = np.zeros( NumberOfCells, dtype=FloatType)